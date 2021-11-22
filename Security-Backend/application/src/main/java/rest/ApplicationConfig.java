a72e239ffd9a8325a67500ca5671817623739755
Haugaard <mathiashn9@gmail.com>
2021-11-22 13:49:01 +0100

    config test

--- Security-Backend/application/src/main/java/rest/ApplicationConfig.java
+++ Security-Backend/application/src/main/java/rest/ApplicationConfig.java
@@ -3,7 +3,11 @@ package rest;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-@javax.ws.rs.ApplicationPath("api")
+/**
+ *
+ * @author Nicklas Nielsen
+ */
+@javax.ws.rs.ApplicationPath("/")
 public class ApplicationConfig extends Application {
 
     @Override
@@ -13,25 +17,19 @@ public class ApplicationConfig extends Application {
         return resources;
     }
 
-    /**
-     * Do not modify addRestResourceClasses() method. It is automatically
-     * populated with all resources defined in the project. If required, comment
-     * out calling this method in getClasses().
-     */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(cors.CorsFilter.class);
         resources.add(errorhandling.mappers.API_ExceptionMapper.class);
         resources.add(errorhandling.mappers.AuthenticationExceptionMapper.class);
         resources.add(errorhandling.mappers.GenericExceptionMapper.class);
         resources.add(errorhandling.mappers.NotAuthorizedExceptionMapper.class);
-        resources.add(errorhandling.mappers.UserCreationExceptionMapper.class);
         resources.add(errorhandling.mappers.UserNotFoundExceptionMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(rest.AuthenticationResource.class);
-        resources.add(rest.MessageResource.class);
+        
         resources.add(rest.UserResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.RolesAllowedFilter.class);
+
     }
 
 }
